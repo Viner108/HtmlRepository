929a171ed5da7961fe5d1bd5520b23a77b575fd4
Viner108 <andrukovichl@mail.ru>
2023-11-09 13:20:47 +0300

    Initial commit

---
+++ src/test/java/com/example/htmlrepository/HtmlRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htmlrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
